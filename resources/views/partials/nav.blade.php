aa6a843fbf68cc7c51afcf322ea93f566aeff1da
Pius Wanjiru <piusmuriithi1@gmail.com>
2016-10-26 13:51:25 +0300

    Adds parallax

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,25 @@
+<header>
+<nav class="navbar navbar-inverse" role="navigation">
+<!-- Brand and toggle get grouped for better mobile display -->
+<div class="navbar-header">
+       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+             <span class="sr-only">Toggle navigation</span>
+                 <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+        </button>
+      <img src="{{ asset('image/ecp_logo.png') }}" />
+</div> 
+<!-- Collect the nav links, forms, and other content for toggling -->
+       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                  <li class="scroll"><a href="/">Home</a></li>
+                  <li class="scroll"><a href="/about">About</a></li>
+                  <li class="scroll"><a href="/services">Services</a></li>
+                  <li class="scroll"><a href="/help">Help</a></li>
+                  <li class="scroll"><a href="/contact">Contact</a></li>
+                  <li><a href="/login">Login</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+</nav>
+    </header>
